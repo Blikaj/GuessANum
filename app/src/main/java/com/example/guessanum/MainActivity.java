2f7f66253fb822a47078034194b2f74f4cce36f3
Blikaj <alex.vs1910@gmail.com>
2021-11-25 12:09:01 +0300

    MD1.5 Added main menu

--- app/src/main/java/com/example/guessanum/MainActivity.java
+++ app/src/main/java/com/example/guessanum/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.guessanum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     Button lvlthree;
     TextView answ;
     TextView guessNumber;
+    TextView tv;
+    CheckBox chb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +44,45 @@ public class MainActivity extends AppCompatActivity {
         answ = (TextView) findViewById(R.id.answertext);
         level = 10;
         guessNumber = (TextView) findViewById(R.id.textView2);
+        tv = (TextView) findViewById(R.id.textView);
+        chb = (CheckBox) findViewById(R.id.chbExtMenu);
+    }
+
+    public void changedif(int lvl){
+        String s;
+        switch (lvl){
+            case (10):
+                level = 10;
+                answ.setVisibility(View.INVISIBLE);
+                s = "Угадайте число от 1 до 10";
+                guessNumber.setText(s.toCharArray(), 0, s.length());
+                break;
+            case (50):
+                level = 50;
+                answ.setVisibility(View.INVISIBLE);
+                s = "Угадайте число от 1 до 50";
+                guessNumber.setText(s.toCharArray(), 0, s.length());
+                break;
+            case(100):
+                level = 100;
+                answ.setVisibility(View.INVISIBLE);
+                s = "Угадайте число от 1 до 100";
+                guessNumber.setText(s.toCharArray(), 0, s.length());
+                break;
+        }
+
     }
 
     public void levelone(View v) {
-        level = 10;
-        answ.setVisibility(View.INVISIBLE);
-        String s;
-        s = "Угадайте число от 1 до 10";
-        guessNumber.setText(s.toCharArray(), 0, s.length());
+        changedif(10);
     }
 
     public void leveltwo(View v) {
-        level = 50;
-        answ.setVisibility(View.INVISIBLE);
-        String s;
-        s = "Угадайте число от 1 до 50";
-        guessNumber.setText(s.toCharArray(), 0, s.length());
+        changedif(50);
     }
 
     public void levelthree(View v) {
-        level = 100;
-        answ.setVisibility(View.INVISIBLE);
-        String s;
-        s = "Угадайте число от 1 до 100";
-        guessNumber.setText(s.toCharArray(), 0, s.length());
+        changedif(100);
     }
 
     public void sendaguess(View v){
@@ -82,4 +105,43 @@ public class MainActivity extends AppCompatActivity {
         answ.setText(s.toCharArray(), 0, s.length());
         answ.setVisibility(View.VISIBLE);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // TODO Auto-generated method stub
+        StringBuilder sb = new StringBuilder();
+
+        // Выведем в TextView информацию о нажатом пункте меню
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        tv.setText(sb.toString());
+        int idd = item.getItemId();
+
+        switch (idd) {
+            case (R.id.menu_levelone):
+                changedif(10);
+                return super.onOptionsItemSelected(item);
+            case (R.id.menu_leveltwo):
+                changedif(50);
+                return super.onOptionsItemSelected(item);
+            case (R.id.menu_levelthree):
+                changedif(100);
+                return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
